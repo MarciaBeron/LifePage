9a51cdd35170a401fd4f4fbad908485a9e1dc68d
Marcia Beron <mtatianabp@gmail.com>
2023-11-08 15:30:33 -0300

    Form and window alerts

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 
+window.alert("This page is a constant project and it is always under construction!\nBe nice!\n\nEsta pagina es un proyecto constante que se encuentra bajo construcción todo el tiempo\n¡Sé amable!")
+
 let toggle = document.getElementById("switch");
 toggle.addEventListener('click', () =>{
     document.body.classList.toggle('light')
+})
+
+let send = document.getElementById("send");
+send.addEventListener('click', () =>{
+    window.alert("Sorry! It doesn't work- yet!\nWe are working on it!\n\n¡Ups! Aún no funciona como debería :(\n¡Estamos trabajando en ello!")
 })
